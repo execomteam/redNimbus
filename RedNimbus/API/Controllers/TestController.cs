a29a843a39bf37b3203dac4387b387aedf59112b
minamaras <minamaras@outlook.com>
2019-08-07 11:33:27 +0200

    Added test controller

---
+++ RedNimbus/API/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("[action]")]
+
+        public string Testiraj(string one, string two)
+        {
+            return one + two;
+        }
+
+        [HttpGet("[action]")]
+        public string Plain()
+        {
+            return "Caooo";
+        }
+
+    }
+}
