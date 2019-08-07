b7c0097cab1d6b23e28f9fd5074bcc943702ede5
pedjasando <sandopredrag95@gmail.com>
2019-08-07 16:28:40 +0200

    JWT auth added and reorganized

--- RedNimbus/API/Services/UserService.cs
+++ RedNimbus/API/Services/UserService.cs
@@ -1,8 +1,13 @@
-﻿using RedNimbus.API.DTO;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RedNimbus.API.DTO;
 using RedNimbus.API.Model;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RedNimbus.API.Services
@@ -10,24 +15,27 @@ namespace RedNimbus.API.Services
     public interface IUserService
     {
         User Create(User user);
-        UserLoginDTO Login(UserLoginDTO udto);
+        UserDTO Login(UserLoginDTO userLoginDTO);
     }
 
     public class UserService : IUserService
     {
-        private Dictionary<string, User> registratedUsers;
+        private static readonly Dictionary<string, User> registeredUsers = new Dictionary<string, User>();
+        private readonly JwtConfiguration jwtConfiguration;
 
-        public UserService()
-        { 
-            registratedUsers = new Dictionary<string, User>();
+        public UserService(IOptions<JwtConfiguration> config)
+        {
+            jwtConfiguration = config.Value;
         }
+      
+        #region registration
 
         private bool CheckRegistrationData(User user)
         {
             if (String.IsNullOrWhiteSpace(user.Email)
                 || String.IsNullOrWhiteSpace(user.Password)
                 || String.IsNullOrWhiteSpace(user.FirstName)
-                || String.IsNullOrWhiteSpace(user.SecondName))
+                || String.IsNullOrWhiteSpace(user.LastName))
             {
                 return false;
             }
@@ -41,27 +49,46 @@ namespace RedNimbus.API.Services
                 return null;
             }
 
-            if (registratedUsers.ContainsKey(user.Email))
+            if (registeredUsers.ContainsKey(user.Email))
             {
                 return null;
             }
 
-            user.Password = HashServices.ComputeSha256Hash(user.Password);
-            registratedUsers.Add(user.Email, user);
+            user.Password = HashService.ComputeSha256Hash(user.Password);
+            registeredUsers.Add(user.Email, user);
             return user;
         }
 
-        public UserLoginDTO Login(UserLoginDTO udto)
+        #endregion
+
+        #region login
+        private string GenerateJwt()
         {
-            if (registratedUsers.ContainsKey(udto.Email))
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.Key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(jwtConfiguration.Issuer,
+              jwtConfiguration.Issuer,
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public UserDTO Login(UserLoginDTO userLoginDTO)
+        {
+            if (registeredUsers.ContainsKey(userLoginDTO.Email))
             {
-                var tempUser = registratedUsers[udto.Email];
-                if (tempUser.Password == HashServices.ComputeSha256Hash(udto.Password))
+                var user = registeredUsers[userLoginDTO.Email];
+                if (user.Password == HashService.ComputeSha256Hash(userLoginDTO.Password))
                 {
-                    return udto;
+                    return new UserDTO(user.FirstName, user.LastName, user.Email, GenerateJwt());
                 }
             }
             return null;
         }
+
+        #endregion
     }
 }
--- RedNimbus/API/Startup.cs
+++ RedNimbus/API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,9 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace RedNimbus.API
 {
+    public class JwtConfiguration
+    {
+        public string Issuer { get; set; }
+        public string Key { get; set; }
+
+    }
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -20,6 +30,20 @@ namespace RedNimbus.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
@@ -27,6 +51,9 @@ namespace RedNimbus.API
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.Configure<JwtConfiguration>(Configuration.GetSection("Jwt"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +79,8 @@ namespace RedNimbus.API
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+
+            app.UseAuthentication();
 
             app.UseSpa(spa =>
             {
