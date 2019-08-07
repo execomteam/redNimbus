c7783e26f5f0849976f62bdf55b01e1f4bde4407
djordjestupar <djordje123stupar@gmail.com>
2019-08-07 11:11:43 +0200

    Login form

--- RedNimbus/API/ClientApp/src/App.js
+++ RedNimbus/API/ClientApp/src/App.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import RegisterForm, {Messages ,WelcomePage} from './RegistrationForm';
+import LoginForm from './LoginForm';
 
 
 function App(){
   
   return(
     <div id="1">
-      <WelcomePage userName="Emil" userSurname="Nisner Bajin"/>
-      <Messages messages="Password empty"/>
-      <RegisterForm/>
+          <WelcomePage userName="Emil" userSurname="Nisner Bajin"/>
+          <Messages messages="Password empty" />
+          <LoginForm />
+          <RegisterForm/>
     </div>
   );
 }
