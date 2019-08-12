6a88b98418088c30fe0684bfefb6525784564736
djordjestupar <djordje123stupar@gmail.com>
2019-08-12 11:42:18 +0200

    Added No button on SignOut page.

--- RedNimbus/API/ClientApp/src/components/SignOut.js
+++ RedNimbus/API/ClientApp/src/components/SignOut.js
@@ -4,11 +4,16 @@ import { withRouter } from "react-router-dom";
 class SignOut extends React.Component{
     constructor(props) {
         super(props);
-        this.setAnswer = this.setAnswer.bind(this);
+        this.setAnswerToYes = this.setAnswerToYes.bind(this);
+        this.setAnswerToNo = this.setAnswerToNo.bind(this);
     }
 
-    setAnswer() {
+    setAnswerToYes() {
         this.props.signOut();
+        this.props.history.push('/');
+    }
+
+    setAnswerToNo() {
         this.props.history.push('/');
     }
 
@@ -18,7 +23,8 @@ class SignOut extends React.Component{
                 <div className="card login-form">
                     <div className="card-body">
                         <h3 className="card-title text-center">Are you sure that you want to sign out?</h3>
-                        <button className="btn btn-primary btn-block" onClick={this.setAnswer}>Yes</button>
+                        <button className="btn btn-primary btn-block" onClick={this.setAnswerToYes}>Yes</button>
+                        <button className="btn btn-block" onClick={this.setAnswerToNo}>No</button>
                     </div>
                 </div>
             </div>
