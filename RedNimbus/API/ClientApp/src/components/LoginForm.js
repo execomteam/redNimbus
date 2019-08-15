7610d65db080f3f2bda8903ff27af6df5507a975
minamaras <minamaras@outlook.com>
2019-08-15 09:28:40 +0200

    Frontend code refactored

--- RedNimbus/API/ClientApp/src/components/LoginForm.js
+++ RedNimbus/API/ClientApp/src/components/LoginForm.js
@@ -8,22 +8,12 @@ class LoginForm extends React.Component {
         super(props);
         this.state = { email: 'enisnerbajin@gmail.com', password: '@Testiranje97' };
 
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.redirectToRegistration = this.redirectToRegistration.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleResponse = this.handleResponse.bind(this);
         this.handleError = this.handleError.bind(this);
     }
 
-    handleEmailChange(event) {
-        this.setState({ email: event.target.value, password: this.state.password });
-    }
-
-    handlePasswordChange(event) {
-        this.setState({ email: this.state.email, password: event.target.value });
-    }
-    
     handleResponse(resp) {
         this.props.changeState(resp);
         this.props.history.push("/");
@@ -40,7 +30,7 @@ class LoginForm extends React.Component {
     handleSubmit(event) {
         
         let self=this;
-        axios.post('http://localhost:65000/api/user/authenticate', { email: this.state.email, password: this.state.password }).then(
+        axios.post('http://localhost:65001/api/user/authenticate', { email: this.state.email, password: this.state.password }).then(
             (response)=>{self.handleResponse(response)}, 
             (response)=>{self.handleError(response)}
 
@@ -63,7 +53,6 @@ class LoginForm extends React.Component {
                                            className="form-control form-control-sm"
                                            id="email"
                                            value={this.state.email}
-                                           onChange={this.handleEmailChange}
                                            placeholder = "Enter e-mail"
                                            required
                                            />
@@ -74,7 +63,6 @@ class LoginForm extends React.Component {
                                     className="form-control form-control-sm"
                                            id="password"
                                            value={this.state.password}
-                                           onChange={this.handlePasswordChange}
                                            placeholder = "Enter password"
                                            required
                                            />
