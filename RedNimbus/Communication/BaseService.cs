a45791433f979fe31dfa531992f4f52876ab8031
fivkovic <fivkovic@uns.ac.rs>
2019-08-22 11:44:25 +0200

    Implemented BaseService. Updated summaries.

--- RedNimbus/Communication/BaseService.cs
+++ RedNimbus/Communication/BaseService.cs
@@ -9,41 +9,83 @@ namespace RedNimbus.Communication
     /// <summary>
     /// Defines the most basic functionalities of a service.
     /// </summary>
-    public abstract class BaseService
+    public abstract class BaseService : IDisposable
     {
-        private readonly string _publisherAddress = "";
-        private readonly string _dealerAddress = "";
+        private const string _publisherAddress = "";
+        private const string _dealerAddress = "";
 
         private SubscriberSocket _subscriberSocket;
         private DealerSocket _dealerSocket;
+        private NetMQPoller _poller;
+
+        private Dictionary<string, Action<NetMQMessage>> _topicsToActions;
+        
+        /// <summary>
+        /// BaseService constructor initializing the Subscriber and Dealer sockets.
+        /// </summary>
+        protected BaseService()
+        {
+            _subscriberSocket = new SubscriberSocket();
+            _dealerSocket = new DealerSocket();
+
+            _poller = new NetMQPoller();
+            _poller.Add(_subscriberSocket);
+
+            _topicsToActions = new Dictionary<string, Action<NetMQMessage>>();
+        }
 
         /// <summary>
-        /// Starts the service instance.
+        /// Starts the service instance by attaching the sockets to specified endpoints.
         /// </summary>
         public void Start()
         {
             _subscriberSocket.Connect(_publisherAddress);
             _dealerSocket.Connect(_dealerAddress);
+
+            _subscriberSocket.ReceiveReady += ReceiveMessageEventHandler;
+
+            _poller.Run();
         }
 
         /// <summary>
-        /// Temporarily stops the service instance.
+        /// Temporarily pauses the service instance disconnecting the sockets from specified endpoints.
         /// </summary>
-        public void Stop()
+        public void Pause()
         {
             _subscriberSocket.Disconnect(_publisherAddress);
             _dealerSocket.Disconnect(_dealerAddress);
 
-            // TODO: IDisposable
+            _poller.Stop();
+        }
+
+        /// <summary>
+        /// Shut down the service and clean up resources.
+        /// </summary>
+        public void Stop()
+        {
+            try
+            {
+                _subscriberSocket.Dispose();
+                _dealerSocket.Dispose();
+
+                _poller.Dispose();
+            }
+            finally
+            {
+                NetMQConfig.Cleanup();
+            }
         }
 
         /// <summary>
         /// Subscribe the service to the specified topic to receive messages related to that topic.
         /// </summary>
         /// <param name="topic">String prefix of the topic.</param>
-        public void Subscribe(string topic)
+        /// <param name="action">Action that should be performed when a message for specified topic is received.</param>
+        public void Subscribe(string topic, Action<NetMQMessage> action)
         {
             _subscriberSocket.Subscribe(topic);
+
+            _topicsToActions.Add(topic, action);
         }
 
         /// <summary>
@@ -53,13 +95,67 @@ namespace RedNimbus.Communication
         public void Unsubscribe(string topic)
         {
             _subscriberSocket.Unsubscribe(topic);
+
+            _topicsToActions.Remove(topic);
         }
 
+        /// <summary>
+        /// Sends the message to the event bus using dealer socket.
+        /// </summary>
+        /// <param name="message">Message to be sent.</param>
         public void SendMessage(NetMQMessage message)
         {
-            // TODO: Send event to event bus using dealer socket
-
             _dealerSocket.SendMultipartMessage(message);
         }
+
+        /// <summary>
+        /// This method is invoked each time when the ReceiveReady event
+        /// occurs on the subscriber socket that is attached to a NetMQPoller instance.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReceiveMessageEventHandler(object sender, NetMQSocketEventArgs e)
+        {
+            NetMQMessage receivedMessage = null;
+
+            while(e.Socket.TryReceiveMultipartMessage(ref receivedMessage))
+            {
+                string topic = receivedMessage.Pop().ConvertToString();
+
+                _topicsToActions.TryGetValue(topic, out Action<NetMQMessage> action);
+
+                action?.Invoke(receivedMessage);
+            }
+        }
+
+        #region IDisposable Support
+        private bool _isDisposed = false;
+
+        /// <summary>
+        /// If disposing equals true, the method has been called directly or indirectly by a user's code.
+        /// Managed and unmanaged resources can be disposed.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _subscriberSocket?.Dispose();
+                    _dealerSocket?.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Use this method for releasing unmanaged resources.
+        /// A derived class should not be able to override this method.
+        /// </summary>
+        public void Dispose() => Dispose(true);
+
+        #endregion
     }
 }
