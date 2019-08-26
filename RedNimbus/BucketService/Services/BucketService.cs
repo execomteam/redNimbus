d11d8e02789f79f9599be09a9eb266144bb754e3
djordjestupar <djordje123stupar@gmail.com>
2019-08-26 17:07:26 +0200

    BucketServiceAdded

---
+++ RedNimbus/BucketService/Services/BucketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RedNimbus.Communication;
+
+namespace RedNimbus.BucketService
+{
+    public class BucketService : BaseService
+    {
+        private string _path;
+
+        public BucketService(string path) : base()
+        {
+            _path = path;
+        }
+    }
+}
