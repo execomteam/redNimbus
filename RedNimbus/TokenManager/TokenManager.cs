cfba19cf557bc2251d335f5f224d672f30afa613
pedjasando <sandopredrag95@gmail.com>
2019-09-03 11:41:54 +0200

    -TokenManager Library created -SecretGenerater Project created for generation secret key that is used for encoding token

---
+++ RedNimbus/TokenManager/TokenManager.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace RedNimbus.TokenManager
+{
+    public class TokenManager
+    {
+        private string _secret = "p644qqY/bOVrqjOrWsVoX0plybUeYdDwyUyCpBfEUSe0nGGO8mpRWB4RVkgKFeIFhLH09wNrmg2e18fgRVvqrA==";
+
+        public string GenerateToken(Guid id)
+        {
+            byte[] key = Convert.FromBase64String(_secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, id.ToString("B")) //Convert guid in following string format: "{x-x-x-x}"
+                    }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+
+            return handler.WriteToken(token);
+        }
+
+        public ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+
+                if (jwtToken == null)
+                {
+                    return null;
+                }
+
+                byte[] key = Convert.FromBase64String(_secret);
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public string ValidateToken(string token)
+        {
+            ClaimsPrincipal principal = GetPrincipal(token);
+
+            if (principal == null)
+            {
+                return null;
+            }
+
+            ClaimsIdentity identity;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+
+            Claim idClaim = identity.FindFirst(ClaimTypes.Authentication);
+
+            return idClaim.Value; //returns guid in string representation
+
+        }
+    }
+}
