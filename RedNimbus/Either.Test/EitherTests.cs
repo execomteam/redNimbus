deebf3f361efe5449dc7324544604b0cebb3c74d
pedjasando <sandopredrag95@gmail.com>
2019-09-03 13:55:25 +0200

    - Either.Test project created - NUnit setup

---
+++ RedNimbus/Either.Test/EitherTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Either.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 == 1);
+        }
+    }
+}
