d3bd82ea2d5b50bb28773c5176e655972ee66c89
pedjasando <sandopredrag95@gmail.com>
2019-09-04 09:28:56 +0200

    stat

--- RedNimbus/Either.Test/EitherTests.cs
+++ RedNimbus/Either.Test/EitherTests.cs
@@ -7,6 +7,29 @@ namespace Either.Test
     [TestFixture]
     public class EitherTest
     {
-        
+        public Either<string, int> either = new Right<string, int>(0);
+
+        [Test]
+        public void When_EitherMapAdapter1Called()
+        {
+
+            Assert.That(
+                (Right<string,int>)either
+                    .Map(Increment) == 1);
+        }
+
+        public void When_EitherMapAdapter2Called()
+        {
+            either.Map(Increment);
+        }
+
+        private int Increment(int x)
+        {
+            return ++x;
+        }
+
+
     }
+
+    
 }
