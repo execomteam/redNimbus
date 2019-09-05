f32b82bd3d16d76934f373ebaee63ba81f56c4a7
pedjasando <sandopredrag95@gmail.com>
2019-09-05 15:02:30 +0200

    - map adapter 1 for successed and faild result - map adapter 2 for successed result

--- RedNimbus/Either.Test/EitherTests.cs
+++ RedNimbus/Either.Test/EitherTests.cs
@@ -7,29 +7,69 @@ namespace Either.Test
     [TestFixture]
     public class EitherTest
     {
-        public Either<string, int> either = new Right<string, int>(0);
+        public Either<Error, Dollars> rightDollars = new Right<Error, Dollars>(new Dollars { Amount = 40 });
+        public Either<Error, Euros> rightEuros = new Right<Error, Euros>(new Euros { Amount = 100 });
+
+        public Either<Error, Dollars> leftDollars = new Left<Error, Dollars>(new Error() { Code = Code.Minus });
+
+        //Map adaptert 1
+        [Test]
+        public void When_EitherMap1AdapterCalled_Expect_RightValue()
+        {
+            double restMoney = (Right<Error, double>)rightDollars.Map(Pay10dollars);           
+            Assert.That( restMoney == 30);
+        }
 
         [Test]
-        public void When_EitherMapAdapter1Called()
+        public void When_EitherMap1AdaptersCalled_Expect_LeftValue()
         {
+            Error error = (Left<Error, double>)leftDollars.Map(Pay10dollars);
+            Assert.That(error.Code == Code.Minus);
 
-            Assert.That(
-                (Right<string,int>)either
-                    .Map(Increment) == 1);
         }
 
-        public void When_EitherMapAdapter2Called()
+        //Map adapter 2
+        [Test]
+        public void WhenEitherMap2AdapterCalled_Expect_RightValue()
         {
-            either.Map(Increment);
+            Dollars exchanged = (Right<Error, Dollars>)rightEuros.Map(ExchangeEuroToDollars);
+            Assert.That(exchanged.Amount == 80);
         }
 
-        private int Increment(int x)
+        
+
+
+
+        private double Pay10dollars(Dollars money)
         {
-            return ++x;
+            return money.Amount -= 10;
         }
 
+        private Either<Error, Dollars> ExchangeEuroToDollars(Euros euros)
+        {
+            return new Right<Error, Dollars>(new Dollars() { Amount = euros.Amount * 0.8 });
+        }
 
     }
 
-    
+    public class Dollars
+    {
+        public double Amount { get; set; }
+    }
+
+    public class Euros
+    {
+        public double Amount { get; set; }
+    }
+
+    public class Error
+    {
+        public Code Code { get; set; }
+    }
+
+    public enum Code
+    {
+        OnZero = 0,
+        Minus = 1
+    }
 }
