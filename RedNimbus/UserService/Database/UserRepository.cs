fc5856816015d6cc839b3c32e0c773ac8413c3cd
Emil <enisnerbajin@gmail.com>
2019-09-09 14:23:28 +0200

    Revert ""Test  class added""
    This reverts commit 3d9d8a6b5e988e662479ac9e9fac62d26bf8cbca.

--- RedNimbus/UserService/Database/UserRepository.cs
+++ RedNimbus/UserService/Database/UserRepository.cs
@@ -80,16 +80,5 @@ namespace UserService.Database
         {
             return _mapper.Map<UserDB>(user);
         }
-
-        public void RemoveUser(string userEmail)
-        {
-            UserDB user;
-            using (context = new DatabaseContext())
-            {
-                user = context.Users.First(u => u.Email.Equals(userEmail));
-                context.Remove(user);
-                context.SaveChanges();
-            }
-        }
     }
 }
