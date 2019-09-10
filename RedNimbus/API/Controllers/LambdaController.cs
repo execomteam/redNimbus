6640adcf5c6cdd9a9f8039b688c78a600428478a
pedjasando <sandopredrag95@gmail.com>
2019-09-10 12:01:06 +0200

    start implementing Api gtw functionality for getLambda feature

---
+++ RedNimbus/API/Controllers/LambdaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedNimbus.API.Controllers;
+using RedNimbus.API.Services;
+using RedNimbus.Either;
+using RedNimbus.Either.Errors;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LambdaController : BaseController
+    {
+        public LambdaService _lambdaService;
+        public LambdaController(LambdaService lambdaService)
+        {
+            _lambdaService = lambdaService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return _lambdaService.GetLambda()
+                .Map(() => AllOk())
+                .Reduce(NotFoundErrorHandler, e => e is NotFoundError)
+                .Reduce(InternalServisErrorHandler);
+        }
+    }
+}
---
+++ RedNimbus/API/Services/LambdaService.cs
@@ -0,0 +1,42 @@
+﻿using NetMQ;
+using RedNimbus.API.Helper;
+using RedNimbus.Communication;
+using RedNimbus.Either;
+using RedNimbus.Either.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedNimbus.API.Services
+{
+    public class LambdaService : BaseService
+    {
+        public Either<IError, string> GetLambda()
+        {
+            Message<LambdaMessage> message = new Message<LambdaMessage>("lambda/get")
+            {
+                Data = new LambdaMessage()
+                {
+                    ...
+                }
+            }
+
+            NetMQMessage temp = message.ToNetMQMessage();
+            NetMQFrame topic = temp.Pop();
+            NetMQFrame empty = temp.Pop();
+            temp.Push(topic);
+
+            NetMQMessage response = RequestSocketFactory.SendRequest(temp);
+            string responseTopic = response.First.ConvertToString();
+
+            if (responseTopic.Equals("Response"))
+            {
+                Message<LambdaMessage> successMessage = new Message<LambdaMessage>(response);
+                return successMessage;
+                
+            }
+            return GetError(response);
+        }
+    }
+}
