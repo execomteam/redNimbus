a8219238d4ad7473d0a9c6a25dae7b268d5cd870
pedjasando <sandopredrag95@gmail.com>
2019-09-16 15:58:56 +0200

    start senderLibrary

--- RedNimbus/Logger/Logger.cs
+++ RedNimbus/Logger/Logger.cs
@@ -1,8 +1,10 @@
-﻿using NetMQ;
+﻿using Google.Protobuf;
+using NetMQ;
 using NetMQ.Sockets;
 using RedNimbus.Messages;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LoggerService
@@ -11,19 +13,45 @@ namespace LoggerService
     {
     }
 
-    public class LogSender
+    public class LogSender : IDisposable
     {
         private DealerSocket _sender;
-        public LogSender()
+
+        public LogSender(string loggerEndpoint)
         {
-            _sender = new DealerSocket("tcp://localhost:8010");
+            _sender = new DealerSocket();
+            _sender.Connect(loggerEndpoint);
+        }
+
+        public void Dispose()
+        {
+            _sender.Close();
         }
 
         public void Send(string requestId, LogMessage log)
         {
             NetMQMessage message = new NetMQMessage();
             NetMQFrame idFrame = new NetMQFrame(requestId);
-            NetMQFrame logFrame = new NetMQFrame();
+            NetMQFrame dataFrame; 
+
+            using (var stream = new MemoryStream()) {
+                log.WriteTo(stream);
+                dataFrame = new NetMQFrame(stream.ToArray()); 
+            }
+
+            message.Append(idFrame);
+            message.Append(dataFrame);
+
+            _sender.SendMultipartMessage(message);
+        }
+    }
+
+    public class LogReceiver
+    {
+        private DealerSocket _receiver;
+        public LogReceiver()
+        {
+
         }
     }
 }
