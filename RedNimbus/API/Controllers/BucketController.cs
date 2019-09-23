0ecb38b14cfa650b2e48d25f1c91aacecda33b1e
pedjasando <sandopredrag95@gmail.com>
2019-09-23 14:03:04 +0200

    logger applied on bucket

--- RedNimbus/API/Controllers/BucketController.cs
+++ RedNimbus/API/Controllers/BucketController.cs
@@ -31,7 +31,7 @@ namespace RedNimbus.API.Controllers
             _logSender = logSender;
         }
 
-        private void LogRequest(Guid id, string origin, string payload = null)
+        private void Log(Guid id, string origin, string payload = null, LogMessage.Types.LogType type = LogMessage.Types.LogType.Info)
         {
             _logSender.Send(id, new LogMessage()
             {
@@ -39,7 +39,7 @@ namespace RedNimbus.API.Controllers
                 Time = DateTime.Now.TimeOfDay.ToString(),
                 Type = LogMessage.Types.LogType.Info,
                 Payload = payload == null ? "some file(s)" : payload,
-                Origin = "UserController/Get"
+                Origin = origin
             });
         }
 
@@ -47,22 +47,24 @@ namespace RedNimbus.API.Controllers
         public IActionResult Get()
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/Get - Request received");
 
             return _bucketService.Get(Request.Headers["token"], requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/Get - Request received", x.ToString()))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/Get - Get bucket successful", x.ToString()))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/Get - Can't find bucket", Request.Headers["token"]))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/Get - InternalError", x.Message));
         }
 
         [HttpPost("post")]
         public IActionResult ListBucketContent([FromBody]StringDto val)
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/ListBucketContent - Request received", val.Path + val.Value);
 
             return _bucketService.ListBucketContent(Request.Headers["token"], val.Path, requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/ListBucketContent - Request received", x.Count.ToString() + " file(s)"))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/ListBucketContent - Request received", x.Count.ToString() + " file(s)"))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketController/ListBucketContent - Can't find bucket's content", val.Path + val.Value))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/ListBucketContent - InternalError", val.Path + val.Value));
         }
 
 
@@ -70,22 +72,24 @@ namespace RedNimbus.API.Controllers
         public IActionResult CreateBucket([FromBody]StringDto bucketName)
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/CreateBucket - Request received", bucketName.Path + bucketName.Value);
 
             return _bucketService.CreateBucket(Request.Headers["token"], bucketName, requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/CreateBucket - Request received", x.Path))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/CreateBucket - Request received", x.Path + x.Value))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/CreateBucket - Can't create bucket", bucketName.Path + bucketName.Value))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/CreateBucket - InternalError", bucketName.Path + bucketName.Value));
         }
 
         [HttpPost("deleteBucket")]
         public IActionResult DeleteBucket([FromBody]StringDto bucketName)
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/DeleteBucket - Request received", bucketName.Path + bucketName.Value);
 
             return  _bucketService.DeleteBucket(Request.Headers["token"], bucketName, requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/DeleteBucket - Request received", x.Path))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/DeleteBucket - Request received", bucketName.Path + bucketName.Value))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/DeleteBucket - Can't delete bucket", bucketName.Path + bucketName.Value))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/Delete - InternalError", bucketName.Path + bucketName.Value));
         }
 
 
@@ -94,11 +98,12 @@ namespace RedNimbus.API.Controllers
         public IActionResult UploadFile([FromForm]UploadFileDto uploadFile)
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/UploadFile - Request received", string.Format("{0} bytes received", uploadFile.File.Length));
 
             return _bucketService.UploadFile(Request.Headers["token"], uploadFile, requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/DeleteBucket - Request received", x.Path))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/UploadFile - Request received", string.Format("{0} bytes to upload", x.File.Length)))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/UploadFile - Can't upload bucket", string.Format("{0} bytes to upload", uploadFile.File.Length)))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/UploadFile - InternalError", string.Format("{0} bytes to upload", uploadFile.File.Length)));
         }
 
         [HttpPost("deleteFile")]
@@ -107,20 +112,21 @@ namespace RedNimbus.API.Controllers
             Guid requestId = Guid.NewGuid();
 
             return _bucketService.DeleteFile(Request.Headers["token"], fileName, requestId)
-                .Map((x) => AllOk(x), (x) => LogRequest(requestId, "BucketService/DeleteBucket - Request received", x.Path))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => AllOk(x), (x) => Log(requestId, "BucketControler/DeleteBucket - Request received", x.Path))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/DeleteFile - Can't upload bucket", fileName.Path))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/DeleteFile - InternalError", string.Format("{0} bytes to upload", fileName.Path)));
         }
 
         [HttpPost("downloadFile")]
         public IActionResult DownloadFile([FromBody]StringDto fileName)
         {
             Guid requestId = Guid.NewGuid();
+            Log(requestId, "BucketControler/DownloadFile - Request received", fileName.Path + fileName.Value);
 
             return _bucketService.DownloadFile(Request.Headers["token"], fileName, requestId)
-                .Map((x) => (IActionResult)File(x.File, x.Type, x.Value), (x) => LogRequest(requestId, "BucketService/DeleteBucket - Request received", fileName.Path))
-                .Reduce(NotFoundErrorHandler, x => x is NotFoundError)
-                .Reduce(InternalServisErrorHandler);
+                .Map((x) => (IActionResult)File(x.File, x.Type, x.Value), (x) => Log(requestId, "BucketControler/DownloadBucket - Request received", string.Format("{0} bytes to download", x.File.Length)))
+                .Reduce(NotFoundErrorHandler, x => x is NotFoundError, (x) => Log(requestId, "BucketControler/DownloadFile - Can't upload bucket", fileName.Path + fileName.Value))
+                .Reduce(InternalServisErrorHandler, (x) => Log(requestId, "BucketControler/DownloadFile - InternalError", fileName.Path + fileName.Value));
         }
     }
 }
